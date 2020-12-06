89868af628ca82aeaf80c96de8e92c20e9540a9d
Kevin Cory <kevin@corysite.com>
2020-12-06 16:19:20 -0600

    Added connection to API

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,27 +1,33 @@
+//import { render } from "@testing-library/react"
 import React from "react"
 
-function TodoItem(props) {
-    const completedStyle = {
-        fontStyle: "italic",
-        color: "#d35e0f",
-        opacity: 0.4,
-        textDecoration: "line-through",
+class TodoItem extends React.Component {
+    componentWillUnmount() {
+        alert("Item about to be deleted!");
       }
-      const { completed, id, title } = props.todo
-  return <li className="todo-item">
-      <input 
-        type="checkbox"
-        checked={completed}
-        onChange={() => props.handleChangeProps(id)}
-        />
-    <button onClick={() => props.deleteTodoProps(id)}>
-        Delete
-    </button>
-    <span style={completed ? completedStyle : null}>
-        {title}
-    </span>
+    render() {
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#d35e0f",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          };
+            const { completed, id, title } = this.props.todo
+        return <li className="todo-item">
+            <input 
+                type="checkbox"
+                checked={completed}
+                onChange={() => this.props.handleChangeProps(id)}
+                />
+            <button onClick={() => this.props.deleteTodoProps(id)}>
+                Delete
+            </button>
+            <span style={completed ? completedStyle : null}>
+                {title}
+            </span>
     
-    </li>
+            </li>
+    }
 
 }
 
